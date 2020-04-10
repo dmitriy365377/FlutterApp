3c71a5cc74f435b41dcb072cf66e22f11e30ae25
dmitriy365377 <tigr365377@gmail.com>
2020-04-10 20:02:10 +0300

    connect function & button

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,39 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQeustion() {
+    print('Answer chosen!');
+  }
+
   Widget build(BuildContext context) {
+    var question = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Text('This is my defalut text'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQeustion,
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: () => print('Hello hr manager!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: () {
+                print('I will be Senior developer!');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
