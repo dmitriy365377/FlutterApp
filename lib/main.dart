e21e88ed92ceff4a77395f114f7a75d7f7592c41
dmitriy365377 <tigr365377@gmail.com>
2020-04-13 11:53:04 +0300

    calculate total score

--- lib/main.dart
+++ lib/main.dart
@@ -19,20 +19,38 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Black', 'Red', 'Green', 'White']
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 6},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ]
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+      'answers': [
+        {'text': 'Rabbit', 'score': 5},
+        {'text': 'Snake', 'score': 5},
+        {'text': 'Elephant', 'score': 4},
+        {'text': 'Lion', 'score': 7}
+      ]
     },
     {
       'questionText': 'What\'s your favorite car?',
-      'answers': ['BMW', 'BMW', 'BMW', 'BMW']
+      'answers': [
+        {'text': 'BMW', 'score': 10},
+        {'text': 'BMW', 'score': 10},
+        {'text': 'BMW', 'score': 10},
+        {'text': 'BMW', 'score': 10}
+      ]
     }
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQeustion() {
+  void _answerQeustion(int score) {
+    _totalScore = _totalScore + score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -41,17 +59,16 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("My First App"),
-        ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                answerQeustion: _answerQeustion,
-                questions: _questions,
-                questionIndex: _questionIndex,
-              )
-            : Result()
-      ),
+          appBar: AppBar(
+            title: Text("My First App"),
+          ),
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQeustion: _answerQeustion,
+                  questions: _questions,
+                  questionIndex: _questionIndex,
+                )
+              : Result()),
     );
   }
 }
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -18,8 +18,9 @@ class Quiz extends StatelessWidget {
     return Column(
       children: [
         Question(questions[questionIndex]['questionText']),
-        ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
-          return Answer(answerQeustion, answer);
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(() => answerQeustion(answer['score']), answer['text']);
         }).toList()
       ],
     );
